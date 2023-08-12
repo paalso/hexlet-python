a6acd28189d7c3e12726f34e709c1b5f699641d7
Pavel Soroka <40a.pvl@gmail.com>
2023-08-12 11:41:11 +0300

    Add solution for python-declarative-programming/stream_chunking challenge

--- 11_python-declarative-programming/stream_chunking.py
+++ 11_python-declarative-programming/stream_chunking.py
@@ -1,5 +1,5 @@
 # https://ru.hexlet.io/challenges/python_declarative_programming_intersperse_exercise
-# 
+# https://ru.hexlet.io/code_reviews/431480
 
 # Чанкование потока
 # ==================
@@ -13,13 +13,36 @@ from itertools import count, islice
 
 
 def ichunks(chunk_size, itarable):
-    splitted_itarable = (itarable[i::chunk_size] for i in range(chunk_size))
-    return map(list, zip(*splitted_itarable))
+    next_chunk = []
+    for e in itarable:
+        next_chunk.append(e)
+        if len(next_chunk) == chunk_size:
+            yield next_chunk
+            next_chunk = []
 
+
+# print(list(ichunks(1, [100, 200, 300, 400])))
+# print(list(squares_plus_one(27)))
+# print(ichunks(2, squares_plus_one(27)))
+# print(list(ichunks(2, squares_plus_one(27))))
 
 assert list(ichunks(1, [])) == []
 assert list(ichunks(2, [42])) == []
 assert list(ichunks(1, [100, 200])) == [[100], [200]]
+assert list(ichunks(2, range(5))) == [[0, 1], [2, 3]]
+
+
+def squares_plus_one(n):
+    k = 1
+    while True:
+        next_ = k * k + 1
+        if next_ > n:
+            return
+        k += 1
+        yield next_
+
+
+assert list(ichunks(2, squares_plus_one(27))) == [[2, 5], [10, 17]]
 
 assert list(
     ichunks(3, ['a', 'b', 'c', 'd']),
@@ -35,3 +58,20 @@ for e in sliced:
         break
     print(e)
     i += 1
+
+
+
+# BEGIN
+# def ichunks(size, source):
+#     return map(list, zip(*([iter(source)] * size)))
+    # "iter(source)" получает именно итератор, даже если на вход
+    # был передан iterable (строка, список).
+    #
+    # "[iterator] * n" размножает ссылки на итератор.
+    #
+    # "zip(*l)" пакует в кортежи все первые элементы из
+    # списка источников "l", затем все вторые, и так далее.
+    # Так как все источники для zip, это ссылки на один и тот же
+    # итератор, при переходе от ссылки к ссылке курсор передвигается
+    # всё дальше. Поэтому кортежи содержат последовательные элементы.
+# END
