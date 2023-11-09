95d6271c0ef1a29de60808877fcda698169fc975
Pavel Soroka <40a.pvl@gmail.com>
2023-11-09 11:46:09 +0200

    Add python-classes liskov-substitution exersice solution

---
+++ 25_python-classes/7_liskov-substitution/InMemory.py
@@ -0,0 +1,12 @@
+class InMemory:
+    def __init__(self):
+        self.elements = {}
+
+    def get(self, key):
+        return self.elements.get(key)
+
+    def set(self, key, value):
+        self.elements[key] = value
+
+    def count(self):
+        return len(self.elements)
