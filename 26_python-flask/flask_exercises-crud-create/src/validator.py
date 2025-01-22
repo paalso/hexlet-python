466cd3417ed76043eacd83190d5cb7d3b17557f9
Pavel Soroka <40a.pvl@gmail.com>
2025-01-22 18:21:28 +0200

    Rename a dir

--- 26_python-flask/flask_exercises-crud-create/src/validator.py
+++
@@ -1,7 +0,0 @@
-def validate(post):
-    errors = {}
-    if not post.get('title'):
-        errors['title'] = "Can't be blank"
-    if not post.get('body'):
-        errors['body'] = "Can't be blank"
-    return errors
