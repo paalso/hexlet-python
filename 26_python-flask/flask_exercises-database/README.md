4c07c97a8eb839a66d4122dc4c0c1044efdb3656
Pavel Soroka <40a.pvl@gmail.com>
2025-02-07 13:14:11 +0200

    flask_exercises-database

--- 26_python-flask/flask_exercises-database/README.md
+++ 26_python-flask/flask_exercises-database/README.md
@@ -1,8 +1,8 @@
 
 ### [Курс Python: Веб-разработка (Flask)](https://ru.hexlet.io/courses/python-flask)
 
-- ##### [Cookies](https://ru.hexlet.io/courses/python-flask/lessons/session/theory_unit)
+- ##### [Работа с базой данных в Flask ](https://ru.hexlet.io/courses/python-flask/lessons/database/theory_unit)
 
-    [Упражнение](https://ru.hexlet.io/courses/python-flask/lessons/session/exercise_unit)
+    [Упражнение](https://ru.hexlet.io/courses/python-flask/lessons/database/exercise_unit)
 
-    [Решение](https://ru.hexlet.io/code_reviews/1692992)
+    [Решение](https://ru.hexlet.io/code_reviews/1707855)
--- 26_python-flask/flask_exercises-database/src/app.py
+++ 26_python-flask/flask_exercises-database/src/app.py
@@ -1,16 +1,24 @@
 import os
 from dotenv import load_dotenv
 from flask import (
+    abort,
     Flask,
+    flash,
+    redirect,
     render_template,
+    request,
+    url_for,
 )
 
 from repository import ProductsRepository, get_db
+from validator import validate
+
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL') or ...
+app.config['DATABASE_URL'] = DATABASE_URL
 
 repo = ProductsRepository(get_db(app))
 
@@ -27,7 +35,38 @@ def products():
 
 
 # BEGIN (write your solution here)
+@app.route('/products/<id>')
+def show_product(id):
+    product = repo.find(id)
+    if product:
+        return render_template(
+            'products/show.html',
+            product=product
+        )
+    abort(404)
+
+
 @app.route('/products/new')
 def new_product():
-    return render_template('products/new.html')
+    product = {}
+    errors = {}
+    return render_template(
+        'products/new.html',
+        product=product,
+        errors=errors
+    )
+
+@app.post('/products')
+def products_post():
+    product = request.form.to_dict()
+    errors = validate(product)
+    if errors:
+        return render_template(
+            'products/new.html',
+            product=product,
+            errors=errors
+        ), 422
+
+    repo.save(product)
+    return redirect(url_for('products'))
 # END
