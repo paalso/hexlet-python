4c1a59411012e3669771aad04a43611cc80428e6
Pavel Soroka <40a.pvl@gmail.com>
2025-04-03 19:56:52 +0300

    python-django-basics: templates exersice

--- 29_python-django-basics/simple_blog/README.md
+++ 29_python-django-basics/simple_blog/README.md
@@ -11,3 +11,9 @@
 [Упражение](https://ru.hexlet.io/courses/python-django-basics/lessons/apps/exercise_unit)
 
 [Решение](https://ru.hexlet.io/code_reviews/1759558)
+
+##### [Шаблонизация](https://ru.hexlet.io/courses/python-django-basics/lessons/templates/theory_unit)
+
+[Упражение](https://ru.hexlet.io/courses/python-django-basics/lessons/templates/exercise_unit)
+
+[Решение](https://ru.hexlet.io/code_reviews/1766099)
--- 29_python-django-basics/simple_blog/simple_blog/views.py
+++ 29_python-django-basics/simple_blog/simple_blog/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+TEAM = [
+    {'name': 'Yoda', 'position': 'CEO'},
+    {'name': 'Obi-Wan Kenobi', 'position': 'Senior Developer'},
+    {'name': 'Anakin Skywalker', 'position': 'Junior Developer'},
+    {'name': 'Jar Jar Binks', 'position': 'Trainee'},
+]
 
-# BEGIN (write your solution here)
+
 def index(request):
     return render(request, 'index.html')
 
 
+# BEGIN (write your solution here)
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', context={'team': TEAM})
 # END
