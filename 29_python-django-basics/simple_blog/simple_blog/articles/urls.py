dbbfa499e3df893b752db8aae4f8f9457a9b32aa
Pavel Soroka <40a.pvl@gmail.com>
2025-04-14 18:03:14 +0300

    python-django-basics/views - hexlet-simple-blog: WIP - CBV version

--- 29_python-django-basics/simple_blog/simple_blog/articles/urls.py
+++ 29_python-django-basics/simple_blog/simple_blog/articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from simple_blog.articles import views
 
 urlpatterns = [
-    path('', views.index, name='articles_index'),
+    # path('', views.index, name='articles_index'),
+    path('', views.IndexView.as_view(), name='articles_index'),
 ]
--- 29_python-django-basics/simple_blog/simple_blog/articles/views.py
+++ 29_python-django-basics/simple_blog/simple_blog/articles/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+
+from django.http import HttpResponse
 
 articles = [
-    {'id': 1, 'title': '"How to foo?"', 'author': 'F. BarBaz'},
-    {'id': 2, 'title': '"Force 101"', 'author': 'O-W. Kenobi'},
-    {'id': 3, 'title': '"Top 10 skyscrapers"', 'author': 'K. Kong'},
-    {'id': 4, 'title': '"Top 10 skyscrapers (jp. edition)"', 'author': 'K. Godzilla'},
-    {'id': 5, 'title': '"5 min recepies"', 'author': 'H. Lector'},
+    {'title': '"How to foo?"', 'author': 'F. BarBaz'},
+    {'title': '"Force 101"', 'author': 'O-W. Kenobi'},
+    {'title': '"Top 10 skyscrapers"', 'author': 'K. Kong'},
+    {'title': '"Top 10 skyscrapers (jp. edition)"', 'author': 'K. Godzilla'},
+    {'title': '"5 min recepies"', 'author': 'H. Lector'},
 ]
 
 
-def index(request):
-    return render(request, 'articles/index.html', context={'articles': articles})
+class IndexView(View):
+    @csrf_exempt
+    def get(self, request):
+        return render(
+            request,
+            'articles/index.html',
+            context={
+                'articles': articles
+            }
+        )
