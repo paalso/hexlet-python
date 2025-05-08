71c54b59e9f7e605658028d5a970a183b2fdc70c
Pavel Soroka <40a.pvl@gmail.com>
2025-05-08 20:00:59 +0300

    python-django-basics: show

--- 29_python-django-basics/simple_blog/simple_blog/articles/views.py
+++ 29_python-django-basics/simple_blog/simple_blog/articles/views.py
@@ -6,14 +6,22 @@ from simple_blog.articles.models import Article
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         articles = Article.objects.all()
-        return render(request, 'articles/index.html', context={
+        return render(
+            request,
+            'articles/index.html',
+            context={
             'articles': articles,
-        })
+        }
+    )
 
 
 class ArticleView(View):
     def get(self, request, *args, **kwargs):
         article = get_object_or_404(Article, id=kwargs['id'])
-        return render(request, 'articles/article.html', context={
+        return render(
+            request,
+            'articles/article.html',
+            context={
             'article': article,
-        })
+        }
+    )
