4fe099378246197d5860d62d8fc458f9d013260c
Pavel Soroka <40a.pvl@gmail.com>
2025-08-17 11:03:03 +0300

    WIP

---
+++ 22_http_api/README.md
@@ -0,0 +1,3 @@
+# [HTTP API](https://ru.hexlet.io/courses/http-api)
+
+[Google Docs link](https://docs.google.com/document/d/16QmiU4KGOLJHynSYS1S6GRqcwZ2wmmDtps5eZSh4c9o/edit?usp=sharing)
--- 27_python-advanced-testing/app/src/scripts/main.py
+++ 27_python-advanced-testing/app/src/scripts/main.py
@@ -1,16 +1,15 @@
 from dotenv import load_dotenv
 import os
 from src.webutils import get_repos_full_names, get_repos
+from src.utils import build_user
+
+
+def build_users(count=10):
+    return [build_user() for _ in range(count)]
 
 
 def main():
-    load_dotenv()
-    token = os.getenv('GITHUB_TOKEN')
-
-    repos = get_repos(token)
-    print(repos)
-    print(get_repos_full_names(token))
-    print(type(repos[0]))
+    print(*build_users(), sep='\n')
 
 
 if __name__ == "__main__":
